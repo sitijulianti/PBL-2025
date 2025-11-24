bcb057f433bc12e70a6075143b215580c1bee673
Siti Julianti <114328719+sitijulianti@users.noreply.github.com>
2025-11-24 08:20:22 +0700

    Delete latihan-02.js

--- latihan-02.js
+++
@@ -1,6 +0,0 @@
-// menyimpan process.argv[2] di konstanta nama
-const nama = process.argv[2];
-const alamat = process.argv[3];
-
-// tampilkan hasil di terminal
-console.log("Selamat Datang, " + nama, "Dari " + alamat);
